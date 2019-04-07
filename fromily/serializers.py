6b41bf53c4bae041b808ae3fcbd098e1bf5e072b
Alexander Dang-Tran <alex.dangtran@gmail.com>
2019-04-07 21:58:13 +0200

    Remove read_only on id
    This was preventing me from creating new users

--- fromily/serializers.py
+++ fromily/serializers.py
@@ -2,15 +2,11 @@ from fromily.models import DiscordUser, DiscordServer, UserServerData
 from rest_framework import serializers
 
 class DiscordServerSerializer(serializers.ModelSerializer):
-    id = serializers.PrimaryKeyRelatedField(read_only=True)
-
     class Meta:
         model = DiscordServer
         fields = ('id', 'server_str', 'dictator')
 
 class DiscordUserSerializer(serializers.ModelSerializer):
-    id = serializers.PrimaryKeyRelatedField(read_only=True)
-
     class Meta:
         model = DiscordUser
         fields = ('id', 'user_str')
